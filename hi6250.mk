ae23040b139056a6dee946ef67d63844b519769b
DarkJoker360 <simoespo159@gmail.com>
2020-09-01 04:16:29 -0700

    hi6250-common: Don't copy duplicate com.android.nfc_extras permissions
    Change-Id: I0ad4b6e25aed3a977e8f62978c7f259ccd6f00bb

--- hi6250.mk
+++ hi6250.mk
@@ -75,8 +75,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 # Radio
 PRODUCT_PACKAGES += \
